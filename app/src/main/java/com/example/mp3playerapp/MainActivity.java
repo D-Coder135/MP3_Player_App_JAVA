6d05ec0d818e3810490c772c81a042b0612f337f
Devansh Shukla <59922056+D-Coder135@users.noreply.github.com>
2021-07-02 23:22:17 +0530

    initialize the object button1 with the method call of the method findViewById()

--- app/src/main/java/com/example/mp3playerapp/MainActivity.java
+++ app/src/main/java/com/example/mp3playerapp/MainActivity.java
@@ -14,5 +14,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        button1 = (Button)findViewById(R.id.button1);
+
     }
 }
