da5964ee2a0e1302ad41e426a11317484f1fd413
Devansh Shukla <59922056+D-Coder135@users.noreply.github.com>
2021-07-02 23:28:03 +0530

    call the pause() method along with the object mediaPlayer

--- app/src/main/java/com/example/mp3playerapp/MainActivity.java
+++ app/src/main/java/com/example/mp3playerapp/MainActivity.java
@@ -28,7 +28,7 @@ public class MainActivity extends AppCompatActivity {
         button2.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-
+                mediaPlayer.pause();
             }
         });
     }
