0881064b5fbda949fdec8eeec0ce3bba8b4dc76c
Devansh Shukla <59922056+D-Coder135@users.noreply.github.com>
2021-07-02 23:20:30 +0530

    define the object for the MediaPlayer class

--- app/src/main/java/com/example/mp3playerapp/MainActivity.java
+++ app/src/main/java/com/example/mp3playerapp/MainActivity.java
@@ -2,11 +2,13 @@ package com.example.mp3playerapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.media.MediaPlayer;
 import android.os.Bundle;
 import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
     Button button1, button2;
+    MediaPlayer mediaPlayer;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
